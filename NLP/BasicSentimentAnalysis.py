e1806e4d6a993d7ee725d099331aad57c4b34bef
BijoySingh <bijoysingh693@gmail.com>
2014-08-14 20:37:24 +0200

    Basic Sentiment Analysis

---
+++ NLP/BasicSentimentAnalysis.py
@@ -0,0 +1,72 @@
+import nltk
+from nltk.tokenize.punkt import PunktWordTokenizer
+
+s = "This is ridiculous"
+s1 = PunktWordTokenizer().tokenize(s)
+print s1
+s2 = nltk.pos_tag(s1)
+print s2
+
+'''
+	1.	CC	Coordinating conjunction
+	2.	CD	Cardinal number
+	3.	DT	Determiner
+	4.	EX	Existential there
+	5.	FW	Foreign word
+	6.	IN	Preposition or subordinating conjunction
+	7.	JJ	Adjective
+	8.	JJR	Adjective, comparative
+	9.	JJS	Adjective, superlative
+	10.	LS	List item marker
+	11.	MD	Modal
+	12.	NN	Noun, singular or mass
+	13.	NNS	Noun, plural
+	14.	NNP	Proper noun, singular
+	15.	NNPS	Proper noun, plural
+	16.	PDT	Predeterminer
+	17.	POS	Possessive ending
+	18.	PRP	Personal pronoun
+	19.	PRP$	Possessive pronoun
+	20.	RB	Adverb
+	21.	RBR	Adverb, comparative
+	22.	RBS	Adverb, superlative
+	23.	RP	Particle
+	24.	SYM	Symbol
+	25.	TO	to
+	26.	UH	Interjection
+	27.	VB	Verb, base form
+	28.	VBD	Verb, past tense
+	29.	VBG	Verb, gerund or present participle
+	30.	VBN	Verb, past participle
+	31.	VBP	Verb, non-3rd person singular present
+	32.	VBZ	Verb, 3rd person singular present
+	33.	WDT	Wh-determiner
+	34.	WP	Wh-pronoun
+	35.	WP$	Possessive wh-pronoun
+	36.	WRB	Wh-adverb
+'''
+
+NOUNS = ["NN","NNS","NNP", "NNPS"]
+ADJECTIVE = ["JJ","JJR","JJS"]
+
+grammar = r"""
+  NP: {<DT|JJ|NN.*>+}          # Chunk sequences of DT, JJ, NN
+  PP: {<IN><NP>}               # Chunk prepositions followed by NP
+  VP: {<VB.*><NP|PP|CLAUSE>+$} # Chunk verbs and their arguments
+  CLAUSE: {<NP><VP>}           # Chunk NP, VP
+  """
+
+cp = nltk.RegexpParser(grammar)
+s3 = cp.parse(s2)
+print s3
+
+nouns = []
+adjective = []
+for (a,b) in s2:
+	if b in NOUNS:
+		nouns.append(a)
+	if b in ADJECTIVE:
+		adjective.append(a)
+
+print nouns
+print adjective
